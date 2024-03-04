b7e0e832a6480278dfcde66843bcb29e86299214
Jacob Kruse <jrkruse@fullsail.student.edu>
2024-03-04 09:52:30 -0500

    Adds Lazy.vim package manager

---
+++ init.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
